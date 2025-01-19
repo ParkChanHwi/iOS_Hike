ea5e939703db2b71f1309b0c3845fb86b6bc8128
Parkchanhwi <cksgnl0523@gmail.com>
2025-01-19 19:07:47 +0900

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by 박찬휘 on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
